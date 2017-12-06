1aba30ffbea51be4cf291d30865000636f377df8
Kevin Neuman <neuman.keg@gmail.com>
2017-12-06 13:29:53 +0200

    Renaming loading and error props

--- src/components/LoadHandler.jsx
+++ src/components/LoadHandler.jsx
@@ -3,12 +3,12 @@ import React, { Component } from 'react';
 class LoadHandler extends Component {
   render() {
     if (this.props.loading) {
-      if (!!this.props.showLoading) return null;
+      if (this.props.hideLoading) return null;
       return <h1>Loading...</h1>;
     }
 
     if (this.props.error) {
-      if (!!this.props.showError) return null;
+      if (this.props.hideError) return null;
       return <h1>{this.props.error.message}</h1>;
     }
 
